5833d284a153691f787cd8d66eac568d54b4f2b2
nshefeek <connectshefeek@gmail.com>
2020-05-06 11:37:08 +0400

    Removed time from Task1

--- P0/Task1.py
+++ P0/Task1.py
@@ -8,27 +8,22 @@ with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-texts_sender = set()
-texts_receiver = set()
+all_numbers = set()
 
 for i in range(len(texts)):
-    texts_sender.add(texts[i][0])
-    texts_receiver.add(texts[i][1])
+    all_numbers.add(texts[i][0])
+    all_numbers.add(texts[i][1])
 
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-a_party = set()
-b_party = set()
-
 for i in range(len(calls)):
-    a_party.add(calls[i][0])
-    b_party.add(calls[i][1])
+    all_numbers.add(calls[i][0])
+    all_numbers.add(calls[i][1])
 
-all_numbers = a_party | b_party | texts_sender | texts_receiver
+#all_numbers = a_party | b_party | texts_sender | texts_receiver
 print(f"There are {len(all_numbers)} different telephone numbers in the records.")
-print(f"Execution time is {end - start:.4f} seconds")
 """
 TASK 1:
 How many different telephone numbers are there in the records?
